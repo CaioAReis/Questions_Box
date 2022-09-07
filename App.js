360593dcb7a54cb7f76caebcf878a12484dc478d
Caio <Caio.almeida16.ca@gmail.com>
2022-09-07 17:42:20 -0300

    Tentando entender o react native paper

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar, SafeAreaView, useColorScheme } from "react-native";
-import { DefaultTheme, configureFonts, Provider as PaperProvider } from "react-native-paper";
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import {
   useFonts,
   WorkSans_100Thin,
@@ -11,25 +11,42 @@ import {
 
 import { Home } from "./src/screens";
 
-const customFonts = {
-  thin: { fontFamily: "WorkSans_100Thin", fontWeight: "normal" },
-  light: { fontFamily: "WorkSans_300Light", fontWeight: "normal" },
-  medium: { fontFamily: "WorkSans_500Medium", fontWeight: "normal" },
-  regular: { fontFamily: "WorkSans_400Regular", fontWeight: "normal" },
+const _customFonts = {
+  regular: { fontFamily: "WorkSans_400Regular" },
+  medium: { fontFamily: "WorkSans_500Medium" },
+  light: { fontFamily: "WorkSans_300Light" },
+  thin: { fontFamily: "WorkSans_100Thin" },
 }
 
 const fontConfig = {
-  default: customFonts,
-  web: customFonts,
-  ios: customFonts,
-  android: customFonts
+  web: _customFonts,
+  ios: _customFonts,
+  android: _customFonts,
+  default: _customFonts,
 };
 
 const theme = {
-  light: {
+  "light": {
     ...DefaultTheme,
     roundness: 2,
-    fonts: configureFonts(fontConfig.android),
+    font: configureFonts(fontConfig),
+    typescale: {
+      bodyLarge: { ...DefaultTheme.typescale.bodyLarge, ..._customFonts.regular },
+      bodyMedium: { ...DefaultTheme.typescale.bodyMedium, ..._customFonts.regular },
+      bodySmall: { ...DefaultTheme.typescale.bodySmall, ..._customFonts.regular },
+      displayLarge: { ...DefaultTheme.typescale.displayLarge, ..._customFonts.regular },
+      displayMedium: { ...DefaultTheme.typescale.displayMedium, ..._customFonts.regular },
+      displaySmall: { ...DefaultTheme.typescale.displaySmall, ..._customFonts.regular },
+      headlineLarge: { ...DefaultTheme.typescale.headlineLarge, ..._customFonts.regular },
+      headlineMedium: { ...DefaultTheme.typescale.headlineMedium, ..._customFonts.regular },
+      headlineSmall: { ...DefaultTheme.typescale.headlineSmall, ..._customFonts.regular },
+      labelLarge: { ...DefaultTheme.typescale.labelLarge, ..._customFonts.medium },
+      labelMedium: { ...DefaultTheme.typescale.labelMedium, ..._customFonts.medium },
+      labelSmall: { ...DefaultTheme.typescale.labelSmall, ..._customFonts.medium },
+      titleLarge: { ...DefaultTheme.typescale.titleLarge, ..._customFonts.regular },
+      titleMedium: { ...DefaultTheme.typescale.titleMedium, ..._customFonts.medium },
+      titleSmall: { ...DefaultTheme.typescale.titleSmall, ..._customFonts.medium },
+    },
     colors: {
       ...DefaultTheme.colors,
       background: "#DCDCE0",
@@ -38,13 +55,15 @@ const theme = {
       secondary: "#9633FF",
       success: "#00A65A",
       error: "#DD4B39",
-      warning: "#DA8C10"
+      warning: "#DA8C10",
+      text: "#191A1F",
     }
   },
-  dark: {
+  "dark": {
     ...DefaultTheme,
-    roundness: 2,
+    roundness: 1,
     fonts: configureFonts(fontConfig),
+    
     colors: {
       ...DefaultTheme.colors,
       background: "#191A1F",
@@ -53,8 +72,12 @@ const theme = {
       secondary: "#9633FF",
       success: "#00A65A",
       error: "#DD4B39",
-      warning: "#DA8C10"
-    }
+      warning: "#DA8C10",
+      text: "#DCDCE0",
+
+      placeholder: "red",
+      underlineColor: "red"
+    },
   },
 };
 
@@ -62,11 +85,7 @@ export default function App() {
   const colorSchema = useColorScheme();
 
   let [fontsLoaded] = useFonts({
-    WorkSans_100Thin,
-    WorkSans_700Bold,
-    WorkSans_300Light,
-    WorkSans_500Medium,
-    WorkSans_400Regular,
+    WorkSans_100Thin, WorkSans_700Bold, WorkSans_300Light, WorkSans_500Medium, WorkSans_400Regular,
   });
 
   if (!fontsLoaded) return null;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,16 +2,35 @@ import React from "react";
 import { View } from "react-native";
 import { Button, Text, useTheme, TextInput } from "react-native-paper";
 
-export const Home = () => {
-  const { colors, fonts } = useTheme();
+export const Home = (props) => {
+  const { colors } = useTheme();
 
   return (
-    <View style={{ backgroundColor: colors.ctt }}>
+    <View style={{ backgroundColor: colors.surface, padding: 20 }}>
 
-      <Text style={{ color: "white", fontFamily: "WorkSans_400Regular" }} >Com a fonte ll</Text>
-      <Text style={{ color: "white" }} >Sem a fonte ll</Text>
-      <TextInput placeholder="Teste ll" />
-      <Button icon="login" mode="contained">Teste ll</Button>
+      {/* <Text style={{ color: "white", fontFamily: "WorkSans_400Regular" }} >Com a fonte ll</Text> */}
+      <Text style={{ color: colors.text, fontFamily: "WorkSans_700Bold", }} >Sem a fonte ll</Text>
+
+      <TextInput
+        label="teste"
+        placeholderTextColor="white"
+        outlineColor={colors.text}
+        style={{ marginBottom: 20, backgroundColor: colors.surface }}
+        mode="outlined"
+        placeholder="Teste ll"
+      />
+
+      <TextInput
+        label="teste"
+        placeholderTextColor="white"
+        outlineColor={colors.text}
+        style={{ marginBottom: 20 }}
+        mode="outlined"
+        placeholder="Teste ll"
+      />
+
+
+      <Button labelStyle={{ fontFamily: "WorkSans_700Bold" }} icon="login" mode="contained">Teste ll</Button>
     </View>
   );
 };
