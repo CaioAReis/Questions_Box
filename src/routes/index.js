49f50d27efc6ccf9bcb797600cdf8ba61ed01fa0
Caio <Caio.almeida16.ca@gmail.com>
2022-09-10 11:05:05 -0300

    Arrumando a pilha de navegação

---
+++ src/routes/index.js
@@ -0,0 +1,105 @@
+import { SafeAreaView, StatusBar, useColorScheme } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer, DefaultTheme as NavigateDefaultTheme } from "@react-navigation/native";
+import {
+  useFonts,WorkSans_100Thin, WorkSans_700Bold, WorkSans_300Light, WorkSans_500Medium, WorkSans_400Regular,
+} from '@expo-google-fonts/work-sans';
+
+import { AuthRoutes } from "./stack.routes";
+
+const Stack = createNativeStackNavigator();
+
+const _defaultFonts = {
+  regular: { fontFamily: "WorkSans_400Regular" },
+  medium: { fontFamily: "WorkSans_500Medium" },
+  light: { fontFamily: "WorkSans_300Light" },
+  thin: { fontFamily: "WorkSans_100Thin" },
+  bold: { fontFamily: "WorkSans_700Bold" }
+}
+
+const theme = {
+  "light": {
+    ...DefaultTheme,
+    ...NavigateDefaultTheme,
+    roundness: 8,
+    dark: false,
+    colors: {
+      ...DefaultTheme.colors,
+      ...NavigateDefaultTheme.colors,
+      text: "#191A1F",
+      error: "#DD4B39",
+      surface: "#FFFFFF",
+      primary: "#191A1F",
+      success: "#00A65A",
+      warning: "#DA8C10",
+      secondary: "#9633FF",
+      outline: "#191A1F50",
+      onSurface: "#191A1F",
+      background: "#ededed",
+      placeholder: "#191A1F",
+      semiWhite: "#191A1F50",
+      onSurfaceVariant: "#191A1F80",
+    },
+    fonts: configureFonts({
+      web: _defaultFonts,
+      ios: _defaultFonts,
+      android: _defaultFonts,
+    }),
+  },
+  "dark": {
+    ...DefaultTheme,
+    ...NavigateDefaultTheme,
+    dark: true,
+    roundness: 8,
+    colors: {
+      ...DefaultTheme.colors,
+      ...NavigateDefaultTheme.colors,
+      text: "#DCDCE0",
+      error: "#DD4B39",
+      primary: "#DCDCE0",
+      success: "#00A65A",
+      surface: "#27314A",
+      warning: "#DA8C10",
+      secondary: "#9633FF",
+      onSurface: "#DCDCE0",
+      background: "#141A28",
+      outline: "#DCDCE050",
+      semiWhite: "#DCDCE050",
+      placeholder: "#DCDCE080",
+      onSurfaceVariant: "#DCDCE080",
+    },
+    fonts: configureFonts({
+      web: _defaultFonts,
+      ios: _defaultFonts,
+      android: _defaultFonts,
+    }),
+  },
+};
+
+const RootRoutes = () => {
+  const colorSchema = useColorScheme();
+
+  let [fontsLoaded] = useFonts({
+    WorkSans_100Thin, WorkSans_700Bold, WorkSans_300Light, WorkSans_500Medium, WorkSans_400Regular,
+  });
+
+  if (!fontsLoaded) return null;
+
+  return (
+    <PaperProvider theme={theme[colorSchema]}>
+      <NavigationContainer theme={theme[colorSchema]}>
+        <SafeAreaView style={{ width: "100%", flex: 1, backgroundColor: theme[colorSchema].colors.background }} >
+          <StatusBar backgroundColor={theme[colorSchema].colors.background} barStyle={colorSchema === "dark" ? "light-content" : "dark-content"} />
+          <Stack.Navigator initialRouteName="Cadastro" screenOptions={{ headerShown: false }} >
+            
+            <Stack.Screen name="AuthRoutes" component={AuthRoutes} />
+
+          </Stack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+export default RootRoutes;
---
+++ src/routes/stack.routes.js
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//  Auth routes
+import { SignIn, SignUp } from "../screens/stack"; 
+
+const Stack = createNativeStackNavigator();
+
+export const AuthRoutes = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
+);
