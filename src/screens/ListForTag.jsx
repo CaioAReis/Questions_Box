f185806810853a51b2122e5dbeaec7621e0f8068
Caio <Caio.almeida16.ca@gmail.com>
2022-10-04 09:20:14 -0300

    Arrumando detalhes

--- src/screens/ListForTag.jsx
+++ src/screens/ListForTag.jsx
@@ -53,28 +53,37 @@ export const ListForTag = ({ route, navigation }) => {
   ];
 
   return (
-    <FlatList
-      data={list}
-      showsVerticalScrollIndicator={false}
-      keyExtractor={question => question._id}
-      contentContainerStyle={{ paddingBottom: 34, backgroundColor: colors.surface }}
-      renderItem={({ item }) => <QuestionCard nav={navigation} question={item} ratio={ratio} />}
-      ListHeaderComponent={
-        <View style={{ padding: 20 }}>
-          <IconButton
-            size={35}
-            color={colors.text}
-            icon="arrow-left-circle-outline"
-            onPress={() => navigation.goBack()}
-            style={{ margin: 0, marginBottom: 20 }}
-          />
-          <TagCard />
-          <Divider style={{ marginVertical: 20 }} />
-          <Title style={{ fontSize: 20 / ratio, marginBottom: 20 }}>
-            Dúvidas com: <Title style={{ color: colors.primary }}>{TAG.title}</Title>
-          </Title>
-        </View>
-      }
-    />
+    <View style={{ backgroundColor: colors.surface, height: "100%" }}>
+      <FlatList
+        data={list}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={question => question._id}
+        contentContainerStyle={{ paddingBottom: 34, backgroundColor: colors.surface }}
+        renderItem={({ item }) => <QuestionCard nav={navigation} question={item} ratio={ratio} />}
+        ListEmptyComponent={
+          <View style={{ padding: 20 }}>
+            <Title style={{ color: colors.semiTransparent, textAlign: "center", fontSize: 20 / ratio, marginBottom: 20 }}>
+              Nenhuma dúvida postada!
+            </Title>
+          </View>
+        }
+        ListHeaderComponent={
+          <View style={{ padding: 20 }}>
+            <IconButton
+              size={35}
+              color={colors.text}
+              icon="arrow-left-circle-outline"
+              onPress={() => navigation.goBack()}
+              style={{ margin: 0, marginBottom: 20 }}
+            />
+            <TagCard />
+            <Divider style={{ marginVertical: 20 }} />
+            <Title style={{ fontSize: 20 / ratio, marginBottom: 20 }}>
+              Dúvidas com: <Title style={{ color: colors.primary }}>{TAG.title}</Title>
+            </Title>
+          </View>
+        }
+      />
+    </View>
   )
 }
