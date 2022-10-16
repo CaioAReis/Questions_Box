995b59898c27c1d913f452c9e49cf926ed8fa253
Caio AReis <Caio.almeida16.ca@gmail.com>
2022-10-16 19:56:26 -0300

    Finalizando o hook de informaçao

--- src/hooks/useInfo.jsx
+++ src/hooks/useInfo.jsx
@@ -1,38 +1,56 @@
-// import { useTheme } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import { Dimensions, PixelRatio, View, StyleSheet } from "react-native";
 import { Button, IconButton, Modal, Text, Title, useTheme } from "react-native-paper";
 
 const ratio = PixelRatio.getFontScale();
-export const UseInfo = () => {
+export const useInfo = () => {
   const { colors } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const [info, setInfo] = useState({ title: "", body: "", type: "" });
 
-  return (
-    <Modal visible={true} style={{ alignItems: "center" }}>
+  const infoTypes = {
+    error: { color: colors.error, icon: "close-circle-outline" },
+    warn: { color: colors.warning, icon: "alert-circle-outline" },
+    success: { color: colors.success, icon: "checkbox-marked-circle-outline" },
+  }
+
+  const showInfo = ( title, body, type ) => {
+    setInfo({ 
+      body: body?.toLocaleLowerCase() ?? "", 
+      type: type?.toLocaleLowerCase() ?? "",
+      title: title?.toLocaleLowerCase() ?? "", 
+    });
+    setIsOpen(true);
+  }
+
+  const InfoContainer = () => (
+    <Modal visible={isOpen} style={{ alignItems: "center" }} onDismiss={() => setIsOpen(false)}>
       <View style={{ ...styles.container, backgroundColor: colors.background }}>
-        <View style={{ ...styles.iconView, backgroundColor: colors.success }}>
-          <IconButton size={120} color="white" icon="checkbox-marked-circle-outline" />
+        <View style={{ ...styles.iconView, backgroundColor: infoTypes[info.type]?.color ?? infoTypes["warn"]?.color }}>
+          <IconButton size={120}  icon={infoTypes[info.type]?.icon ?? infoTypes["warn"]?.icon} />
         </View>
 
         <View style={{ padding: 20 }}>
-          <Title numberOfLines={3} style={styles.title}>
-            Título da informação
-          </Title>
-
-          <Text style={styles.text}>
-            Algum corpo de informação se necessário
-          </Text>
-          <Button color={colors.success} contentStyle={{ height: 45 }} mode="contained">Continuar</Button>
+          <Title numberOfLines={3} style={styles.title}>{Boolean(info.title) ? info.title : ""}</Title>
+          <Text style={styles.text}>{Boolean(info.body) ? info.body : ""}</Text>
+          <Button
+            mode="contained"
+            contentStyle={{ height: 45 }}
+            onPress={() => {
+              setInfo({ title: "", body: "", type: "" });
+              setIsOpen(false);
+            }}
+            color={infoTypes[info.type]?.color ?? infoTypes["warn"]?.color }
+          >
+            Continuar
+          </Button>
         </View>
       </View>
     </Modal>
   );
-}
 
-// export const useInfo = () => {
-//   const { ConfErrBody, startConfErr } = confErr();
-//   return { ConfErrBody, startConfErr };
-// }
+  return { InfoContainer, showInfo };
+}
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -9,8 +9,6 @@ import I3 from "../../assets/Questions.png";
 import I4 from "../../assets/Quiz.png";
 import I5 from "../../assets/Welcome.png";
 
-import { UseInfo } from "../hooks/useInfo";
-
 const ratio = PixelRatio.getFontScale();
 
 export const Welcome = ({ navigation }) => {
@@ -29,79 +27,75 @@ export const Welcome = ({ navigation }) => {
   };
 
   return (
-    <>
-      <View style={styles.container}>
-        <Image source={logos[1]} resizeMode="contain" style={{ marginTop: 40, width: imgDimensions, height: imgDimensions }} />
+    <View style={styles.container}>
+      <Image source={logos[1]} resizeMode="contain" style={{ marginTop: 40, width: imgDimensions, height: imgDimensions }} />
 
-        <ScrollView scrollEnabled={false} ref={scrollRef} showsHorizontalScrollIndicator={false} pagingEnabled horizontal style={{ paddingVertical: 20, flex: 1 }}>
-          <View style={styles.step}>
-            <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
-              <Image resizeMode="contain" source={I2} style={styles.img} />
-            </ImageBackground>
-            <View>
-              <Title style={span}>Olá mundo!</Title>
-              <Title style={title}>Seja muito bem-vindo(a) ao nosso APP</Title>
-            </View>
+      <ScrollView scrollEnabled={false} ref={scrollRef} showsHorizontalScrollIndicator={false} pagingEnabled horizontal style={{ paddingVertical: 20, flex: 1 }}>
+        <View style={styles.step}>
+          <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
+            <Image resizeMode="contain" source={I2} style={styles.img} />
+          </ImageBackground>
+          <View>
+            <Title style={span}>Olá mundo!</Title>
+            <Title style={title}>Seja muito bem-vindo(a) ao nosso APP</Title>
           </View>
-
-          <View style={styles.step}>
-            <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
-              <Image resizeMode="contain" source={I4} style={styles.img} />
-            </ImageBackground>
-            <Title style={title}>
-              A todo momento possuímos <Title style={span}>dúvidas</Title> sobre algo, não é mesmo?
-            </Title>
-          </View>
-
-          <View style={styles.step}>
-            <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
-              <Image resizeMode="contain" source={I3} style={styles.img} />
-            </ImageBackground>
-            <Title style={title}>
-              Esperamos que voçê consiga tirar <Title style={span}>todas</Title> elas aqui!
-            </Title>
-          </View>
-
-          <View style={styles.step}>
-            <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
-              <Image resizeMode="contain" source={I1} style={styles.img} />
-            </ImageBackground>
-            <Title style={title}>
-              Faça <Title style={span}>perguntas</Title> ou tire as dúvidas da galera!
-            </Title>
-          </View>
-
-          <View style={styles.step}>
-            <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
-              <Image resizeMode="contain" source={I5} style={styles.img} />
-            </ImageBackground>
-            <Title style={title}>
-              <Title style={span}>Entre</Title> ou <Title style={span}>cadastre-se</Title> para começar!
-            </Title>
-          </View>
-        </ScrollView>
-
-        <View style={{ paddingBottom: 20, width: "100%" }}>
-          {step < 5 ? (
-            <View style={{ alignItems: "center" }}>
-              <IconButton size={70} icon="arrow-right-circle" color={colors.primary} onPress={handleNextStep} />
-            </View>
-          ) : (
-            <View style={{ paddingHorizontal: 40, paddingTop: 20 }}>
-              <Button contentStyle={{ height: 45 }} style={{ marginBottom: 20 }} icon="login" mode="contained" onPress={() => navigation.navigate("SignIn")}>
-                Fazer login
-              </Button>
-
-              <Button contentStyle={{ height: 45 }} icon="account-arrow-right" mode="text" onPress={() => navigation.navigate("SignUp")}>
-                Fazer cadastro
-              </Button>
-            </View>
-          )}
         </View>
-      </View>
 
-      <UseInfo />
-    </>
+        <View style={styles.step}>
+          <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
+            <Image resizeMode="contain" source={I4} style={styles.img} />
+          </ImageBackground>
+          <Title style={title}>
+            A todo momento possuímos <Title style={span}>dúvidas</Title> sobre algo, não é mesmo?
+          </Title>
+        </View>
+
+        <View style={styles.step}>
+          <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
+            <Image resizeMode="contain" source={I3} style={styles.img} />
+          </ImageBackground>
+          <Title style={title}>
+            Esperamos que voçê consiga tirar <Title style={span}>todas</Title> elas aqui!
+          </Title>
+        </View>
+
+        <View style={styles.step}>
+          <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
+            <Image resizeMode="contain" source={I1} style={styles.img} />
+          </ImageBackground>
+          <Title style={title}>
+            Faça <Title style={span}>perguntas</Title> ou tire as dúvidas da galera!
+          </Title>
+        </View>
+
+        <View style={styles.step}>
+          <ImageBackground imageStyle={{ opacity: 0.7 }} resizeMode="stretch" source={mancha}>
+            <Image resizeMode="contain" source={I5} style={styles.img} />
+          </ImageBackground>
+          <Title style={title}>
+            <Title style={span}>Entre</Title> ou <Title style={span}>cadastre-se</Title> para começar!
+          </Title>
+        </View>
+      </ScrollView>
+
+      <View style={{ paddingBottom: 20, width: "100%" }}>
+        {step < 5 ? (
+          <View style={{ alignItems: "center" }}>
+            <IconButton size={70} icon="arrow-right-circle" color={colors.primary} onPress={handleNextStep} />
+          </View>
+        ) : (
+          <View style={{ paddingHorizontal: 40, paddingTop: 20 }}>
+            <Button contentStyle={{ height: 45 }} style={{ marginBottom: 20 }} icon="login" mode="contained" onPress={() => navigation.navigate("SignIn")}>
+              Fazer login
+            </Button>
+
+            <Button contentStyle={{ height: 45 }} icon="account-arrow-right" mode="text" onPress={() => navigation.navigate("SignUp")}>
+              Fazer cadastro
+            </Button>
+          </View>
+        )}
+      </View>
+    </View>
   );
 };
 
