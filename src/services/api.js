17576e70b167e7d862502191d21acd123e9da409
Caio AReis <Caio.almeida16.ca@gmail.com>
2022-11-17 21:27:51 -0300

    Pegando as rotas da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,69 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "ALGUMA COISA",
+    baseURL: "BASE_DA_URL/br",
     headers: {  }
 });
 
+//      /questions
+//      /response
+//      /users
+//      /tags
+
 export const API = {
+
+    //  QUESTIONS: /questions
+        // Listar perguntas pelas tag
+        //  GET: /questionbytag/:tagId
+
+        // Buscar uma pergunta específica
+        //  GET: /:questionId
+
+        // Criar uma pergunta
+        //  POST: /
+
+        //  Responder uma perguntaResponder uma pergunta
+        //  POST: /response/:questionId
+
+        //  REAÇÂO
+        //  PUT: /like/:questionId
+
+        //  EDITAR RESPOSTA
+        //  PUT: /response/edit/:questionId
+
+    //  USERS: /users
+        //  Login
+        //  POST: /login
+
+        // Detalhes do usuário
+        //  GET: /:id
+
+        //  Listar usuários
+        //  GET: /
+
+        //  Cadastrar usuário
+        //  POST: /
+
+        //  Editar usuário
+        //  PUT: /:id
+
+        //  Revogar usuário
+        //  DELETE: /:id
+
+    //  TAGS: /tags
+        //  Listar Tags
+        //  GET: /list
+
+        //  Listar Tags POPULARES (apenas as 8 primeiras)
+        //  GET: /list/popupar
+
+        //  Cadastrar uma TAG
+        //  POST: /
+
+        //  Apagar TAG
+        //  DELETE: /delete/:id
+        
+
     tagList: async () => {},
     editUser: async () => {},
     createTag: async () => {},
